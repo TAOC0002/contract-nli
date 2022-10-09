3d2136a952b071468267f022213962e3a9130b99
TAOC0002 <taoc0620@gmail.com>
2022-10-09 22:39:57 +0800

    bug fix

--- contract_nli/model/identification_classification/bert.py
+++ contract_nli/model/identification_classification/bert.py
@@ -20,7 +20,6 @@ from transformers.utils import logging
 from contract_nli.dataset.loader import NLILabel
 from contract_nli.model.identification_classification.model_output import \
     IdentificationClassificationModelOutput
-from contract_nli.model.identification_classification.prompt import PromptEncoder
 
 logger = logging.get_logger(__name__)
 
